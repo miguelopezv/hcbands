f767be2249b3acaa25d3721603bd20eb6455dff8
Miguel López <miguellopez@vitalware.com>
2019-07-11 00:55:20 -0500

    update bands module

---
+++ src/lib/bands/bands.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import * as fromContainers from './containers';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: fromContainers.BandsLayoutComponent }
+];
+
+@NgModule({
+  declarations: [...fromContainers.containers],
+  imports: [CommonModule, RouterModule.forChild(routes)]
+})
+export class BandsModule {}
---
+++ src/lib/bands/containers/bands-layout/bands-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bands-layout',
+  templateUrl: './bands-layout.component.html',
+  styleUrls: ['bands-layout.component.scss']
+})
+export class BandsLayoutComponent {}

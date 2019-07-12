856025baa252e7dddab54504b23730df09f5f75e
Miguel López <miguellopez@vitalware.com>
2019-07-11 22:51:07 -0500

    create band layout

---
+++ src/lib/band/containers/band-layout/band-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-band-layout',
+  templateUrl: './band-layout.component.html',
+  styleUrls: ['./band-layout.component.scss']
+})
+export class BandLayoutComponent {}
---
+++ src/lib/band/containers/index.ts
@@ -0,0 +1,5 @@
+import { BandLayoutComponent } from './band-layout/band-layout.component';
+
+export const containers = [BandLayoutComponent];
+
+export * from './band-layout/band-layout.component';

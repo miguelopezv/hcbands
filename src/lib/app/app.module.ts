d35c10d7393b6dd794e9e8021be809f6b3471c27
Miguel López <miguellopez@vitalware.com>
2019-07-11 22:50:22 -0500

    move Fontawesome to another module

--- src/lib/app/app.module.ts
+++ src/lib/app/app.module.ts
@@ -3,20 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
-
 import * as fromComponents from './components';
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [fromComponents.AppComponent]
 })
-export class AppModule {
-  constructor() {
-    library.add(faSpinner);
-  }
-}
+export class AppModule {}
--- src/lib/bands/bands.module.ts
+++ src/lib/bands/bands.module.ts
@@ -4,12 +4,24 @@ import { CommonModule } from '@angular/common';
 import * as fromContainers from './containers';
 import { Routes, RouterModule } from '@angular/router';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faPlus,
+  faSpinner,
+  faExclamation
+} from '@fortawesome/free-solid-svg-icons';
+
 const routes: Routes = [
   { path: '', component: fromContainers.BandsLayoutComponent }
 ];
 
 @NgModule({
   declarations: [...fromContainers.containers],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  imports: [CommonModule, RouterModule.forChild(routes), FontAwesomeModule]
 })
-export class BandsModule {}
+export class BandsModule {
+  constructor() {
+    library.add(faPlus, faSpinner, faExclamation);
+  }
+}

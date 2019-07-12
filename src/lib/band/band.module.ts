7723539657e102bbbb4c34d9e6c5fd694164e96d
Miguel López <miguellopez@vitalware.com>
2019-07-11 22:50:43 -0500

    create routes on band module

--- src/lib/band/band.module.ts
+++ src/lib/band/band.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import * as fromContainers from './containers';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: fromContainers.BandLayoutComponent }
+];
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [...fromContainers.containers],
+  imports: [CommonModule, RouterModule.forChild(routes)]
 })
-export class BandModule { }
+export class BandModule {}

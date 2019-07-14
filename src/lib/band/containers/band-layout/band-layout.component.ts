3e5a780d2348c34033a5b73a36a8d1d503d5cfb9
Miguel López <miguellopez@vitalware.com>
2019-07-14 13:10:25 -0500

    edit and save one band

--- src/lib/band/containers/band-layout/band-layout.component.ts
+++ src/lib/band/containers/band-layout/band-layout.component.ts
@@ -1,35 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Band } from 'src/lib/models/band.model';
-import { NgForm } from '@angular/forms';
 import { BandsService } from '../../../services/bands.service';
+
+import Swal from 'sweetalert2';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-band-layout',
   templateUrl: './band-layout.component.html',
   styleUrls: ['./band-layout.component.scss']
 })
-export class BandLayoutComponent {
-  constructor(private bandsService: BandsService) {
-    // this.bandsService.readBands().subscribe(data => {
-    //   const bands = data.map(e => {
-    //     return {
-    //       id: e.payload.doc.id,
-    //       ...e.payload.doc.data()
-    //     } as Band;
-    //   });
-    //   console.log(bands);
-    // });
-  }
+export class BandLayoutComponent implements OnInit {
   band: Band = { name: '', country: '', status: true };
+  id: string;
+  constructor(
+    private bandsService: BandsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+
+    if (this.id !== 'new') {
+      this.bandsService.readBand(this.id).subscribe(data => {
+        this.band = {
+          id: data.payload.id,
+          ...data.payload.data()
+        } as Band;
+      });
+    }
+  }
 
   save() {
+    let request: Promise<any>;
+
+    Swal.fire({
+      title: 'saving...',
+      type: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
     if (this.band.id) {
-      this.bandsService.updateBand(this.band).then(data => console.log(data));
+      request = this.bandsService.updateBand(this.band);
     } else {
-      this.bandsService
-        .createBand(this.band)
-        .then((id: string) => (this.band.id = id));
+      request = this.bandsService.createBand(this.band);
     }
+
+    request.then((data: any) => {
+      this.band.id = data ? data : this.band.id;
+
+      Swal.fire({
+        title: this.band.name,
+        text: 'data saved',
+        type: 'success'
+      });
+    });
   }
 
   setBandStatus() {

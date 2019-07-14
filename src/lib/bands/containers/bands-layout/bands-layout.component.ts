db4fdee3de6078b27e663813cf9fb3bd5754829e
Miguel López <miguellopez@vitalware.com>
2019-07-14 13:09:39 -0500

    return all bands and delete one

--- src/lib/bands/containers/bands-layout/bands-layout.component.ts
+++ src/lib/bands/containers/bands-layout/bands-layout.component.ts
@@ -1,8 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BandsService } from 'src/lib/services/bands.service';
+import { Band } from 'src/lib/models/band.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-bands-layout',
   templateUrl: './bands-layout.component.html',
   styleUrls: ['bands-layout.component.scss']
 })
-export class BandsLayoutComponent {}
+export class BandsLayoutComponent implements OnInit {
+  bands: Band[];
+  loading = true;
+
+  constructor(private bandsService: BandsService) {}
+
+  ngOnInit() {
+    this.bandsService.readBands().subscribe(data => {
+      this.loading = false;
+      this.bands = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as Band;
+      });
+    });
+  }
+
+  deleteBand(band: Band) {
+    Swal.fire({
+      title: 'delete?',
+      text: `do you want to delete ${band.name}?`,
+      type: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(response =>
+      response.value ? this.bandsService.deleteBand(band.id) : ''
+    );
+  }
+}
